b2bc308d5bf71939ce0e26fddf711fa08b0a66c1
preeti-create <55034143+preeti-create@users.noreply.github.com>
2021-03-09 00:39:17 +0530

    mapping data to component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import Card from "./Card";
+import contacts from "../contacts";
+
+function createCard(contact)
+{
+return(
+<Card 
+key = {contact.id}
+name = {contact.name}
+img = {contact.imgURL}
+phone = {contact.phone}
+email = {contact.email}
+/>
+);
+}
+
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-    <Card
+
+     {contacts.map(createCard)};
+     
+    {/* <Card
     name = "Beyonce"
     img = "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
     tel = "+123 456 789"
@@ -23,7 +42,7 @@ function App() {
     img = "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
     tel = "+918 372 574"
     email = "gmail@chucknorris.com"
-    />
+    /> */}
 
     </div>
   );
